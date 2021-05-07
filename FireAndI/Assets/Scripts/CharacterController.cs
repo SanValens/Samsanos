1e258e30ecfa4f3cf9fe5bf53e00c6eb9c91784b
Santiago Valenzuela <sa_valens@hotmail.com>
2021-05-07 09:38:37 -0500

    Proyecto Unity iniciado

---
+++ FireAndI/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
